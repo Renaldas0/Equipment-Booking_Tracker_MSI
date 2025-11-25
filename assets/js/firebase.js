16eb6bce7e013d708d077c81ec8d26fb0e2b0d01
renaldas <cnf734@motorolasolutions.com>
2025-11-25 11:46:18 +0000

    install firebase packages

---
+++ assets/js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: {apiKey},
+  authDomain: "msi-tracker.firebaseapp.com",
+  projectId: "msi-tracker",
+  storageBucket: "msi-tracker.firebasestorage.app",
+  messagingSenderId: "486848526130",
+  appId: "1:486848526130:web:1fced54f6baa99aa91092b",
+  measurementId: "G-2B53VF2S75"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
