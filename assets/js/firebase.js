9dace9ab4a3f42d59170b759d7bb42a19f644b56
renaldas <cnf734@motorolasolutions.com>
2025-11-25 13:44:05 +0000

    Add Tailwind CLI and Configure Firebase to an anonymous auth user

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: {apiKey},
+  apiKey: "AIzaSyBq0PvSp-00mcbHhWtiCDK9SCtGDVikTL4",
   authDomain: "msi-tracker.firebaseapp.com",
   projectId: "msi-tracker",
   storageBucket: "msi-tracker.firebasestorage.app",
